3d893ad6d3c26cc4439a5ec13fa1120c29f46169
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-08-07 15:15:43 -0700

    Refactor projClickListener.js to hide/show todo items button and show project name dynamically

--- src/projClickListener.js
+++ src/projClickListener.js
@@ -1,7 +1,7 @@
 // Purpose: Add an event listener to each project in the projects list that displays the todoItems for the project that was clicked
 
 import { itemsContent } from "./itemContent";
-import { navButtonSwitch, toDoItemAdd } from "./projectUI";
+import { hideShowTodoItemsBtn, navButtonSwitch, showProjBtn, showProjectH2, toDoItemAdd } from "./projectUI";
 
 export default function addProjectClickListener(todoListApp, content) {
     const projectNames = document.querySelectorAll('h2'); // Get all h2 elements
@@ -9,6 +9,7 @@ export default function addProjectClickListener(todoListApp, content) {
         projectName.addEventListener('click', (e) => { // Add an event listener to each h2 element
             console.log("You clicked on a project."); // Log success
 
+            hideShowTodoItemsBtn(); // Hide the "Show All To Do Items" button
             navButtonSwitch('+ New To Do Item'); // Switch the navbar button text
 
             const clickedProjectName = e.target.textContent; // Get the text content of the clicked h2 element
@@ -21,6 +22,8 @@ export default function addProjectClickListener(todoListApp, content) {
                 console.error("Project not found."); // Error handling if no matching project is found
             }
 
+            showProjectH2(clickedProjectName); // Show the project name in the header
+            showProjBtn(); // Show the projects button
             toDoItemAdd(clickedProject);
         })
     })
